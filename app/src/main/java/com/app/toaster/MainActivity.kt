2562cb8d6bf611ea8ae95049aadb149b68c723e1
Brian_OB <25736452+Brianonchari@users.noreply.github.com>
2021-03-13 12:32:34 +0300

    Refactor

--- app/src/main/java/com/app/toaster/MainActivity.kt
+++ app/src/main/java/com/app/toaster/MainActivity.kt
@@ -2,13 +2,11 @@ package com.app.toaster
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.app.hilibrary.HelloMessage
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
     }
 }
--- hilibrary/src/main/java/com/app/hilibrary/HelloMessage.kt
+++ hilibrary/src/main/java/com/app/hilibrary/HelloMessage.kt
@@ -3,7 +3,7 @@ package com.app.hilibrary
 import android.content.Context
 import android.widget.Toast
 
-object ToasterMessage {
+class ToasterMessage {
     fun s(c: Context?, message: String?) {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
     }
