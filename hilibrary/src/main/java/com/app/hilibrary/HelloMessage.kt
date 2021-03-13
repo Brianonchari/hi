c2c39f38f23c1d8201f9c08308ff53311bd4aa5f
Brian_OB <25736452+Brianonchari@users.noreply.github.com>
2021-03-13 11:11:59 +0300

    Dont bother checking this repo

--- hilibrary/src/main/java/com/app/hilibrary/HelloMessage.kt
+++ hilibrary/src/main/java/com/app/hilibrary/HelloMessage.kt
@@ -3,9 +3,6 @@ package com.app.hilibrary
 import android.content.Context
 import android.widget.Toast
 
-
-
-
 class HelloMessage {
     fun s(c: Context?, message: String?) {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
